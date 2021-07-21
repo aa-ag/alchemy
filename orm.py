4559087bebb7068861a2a94da027599c9fdc9eaf
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-20 21:10:38 -0500

    added base code for orm.py

--- orm.py
+++ orm.py
@@ -1,12 +1,14 @@
 ############------------ IMPORTS ------------############
-
+from sqlalchemy import create_engine
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 
 
 ############------------ FUNCTION(S) ------------############
-
+def create_postgresql_engine():
+    engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
+    
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
-    pass
+    create_postgresql_engine()
