e200f2fd28773d66da2483d97f32cc6e46fc42cc
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-20 21:12:43 -0500

    added logic to orm.py to create Session and Base

--- orm.py
+++ orm.py
@@ -1,5 +1,7 @@
 ############------------ IMPORTS ------------############
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 
@@ -7,7 +9,11 @@ from sqlalchemy import create_engine
 ############------------ FUNCTION(S) ------------############
 def create_postgresql_engine():
     engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
-    
+
+    Session = sessionmaker(bind=engine)
+
+    Base = declarative_base()
+
 
 ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
