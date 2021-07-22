3ed1aef7974b8cb3f5be98664399280aa42d0ffb
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-21 19:37:45 -0500

    refactored orm.py to create Base first and then Product class, importing necessary imports from sqlalchemy beforehand

--- orm.py
+++ orm.py
@@ -2,19 +2,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer, Date, Boolean, Numeric
+
 
 ############------------ GLOBAL VARIABLE(S) ------------############
+engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
 
+Session = sessionmaker(bind=engine)
 
-############------------ FUNCTION(S) ------------############
-def create_postgresql_engine():
-    engine = create_engine('postgresql://usr:pass@localhost:5432/sqlalchemy')
+Base = declarative_base()
 
-    Session = sessionmaker(bind=engine)
-
-    Base = declarative_base()
-
-
-############------------ DRIVER CODE ------------############
-if __name__ == "__main__":
-    create_postgresql_engine()
+class Product(Base):
+    __tablename__ = 'products'
+    id=Column(Integer, primary_key=True)
+    title=Column('title', String(32))
+    in_stock=Column('in_stock', Boolean)
+    quantity=Column('quantity', Integer)
+    price=Column('price', Numeric)
