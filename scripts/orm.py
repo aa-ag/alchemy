2fdf9ae7578d230081e2fbdb03dd9a727e84e054
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-22 18:58:26 -0500

    updated orm.py to reflect F1 db engine creation

--- scripts/orm.py
+++ scripts/orm.py
@@ -6,24 +6,36 @@ from sqlalchemy.orm import relationship, backref
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
-engine = create_engine('sqlite:///student.db', echo=True)
+engine = create_engine('sqlite:///scuderias.db', echo=True)
 
 Base = declarative_base()
 
 ############------------ CLASSES ------------############
-class Student(Base):
-    __tablename__ = "student"
+class Scuderias(Base):
+    __tablename__ = "scuderias"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String)
-    firstname = Column(String)
-    lastname = Column(String)
-    school = Column(String)
+    scuderias_name = Column(String, max_lenght=80)
+    principals_firstname = Column(String, max_lenght=20)
+    principals_lastname = Column(String, max_lenght=20)
+    driver1_firstname = Column(String, max_lenght=20)
+    driver1_lastname = Column(String, max_lenght=20)
+    driver2_firstname = Column(String, max_lenght=20)
+    driver2_lastname = Column(String, max_lenght=20)
+    lastchampionship = Column(Date)
 
-    def __init__(self, username, firstname, lastname, school):
-        self.username = username
-        self.firstname = firstname
-        self.lastname = lastname
-        self.school = school
+    def __init__(self, scuderias_name, \
+        principals_firstname, principals_lastname, \
+        driver1_firstname, driver1_lastname, \
+            driver2_firstname, driver2_lastname, \
+                lastchampionship):
+        self.scuderias_name = scuderias_name
+        self.principals_firstname = principals_firstname
+        self.principals_lastname = principals_lastname
+        self.driver1_firstname = driver1_firstname
+        self.driver1_lastname = driver1_lastname
+        self.driver2_firstname = driver2_firstname
+        self.driver2_lastname = driver2_lastname
+        self.lastchampionship = lastchampionship
 
 Base.metadata.create_all(engine)
