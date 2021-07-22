6969bf3419a08f64ed160b5df927d66a5904b64a
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-21 20:05:39 -0500

    added __init__ method and mapping of parameters

--- orm.py
+++ orm.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, ForeignKey, Column,
 from sqlalchemy import Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.sql.functions import user
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
@@ -11,3 +12,18 @@ engine = create_engine('sqlite:///student.db', echo=True)
 Base = declarative_base()
 
 ############------------ CLASSES ------------############
+class Student(Base):
+    __tablename__ = "student"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    school = Column(String)
+
+    def __init__(self, username, firstname, lastname, school):
+        self.username = username
+        self.firstname = firstname
+        self.lastname = lastname
+        self.school = school
+
